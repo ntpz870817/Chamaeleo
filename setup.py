0fade06013e7e1e03eea384ff1624262fb9e8f5d
HaolingZHANG <hlzchn@gmail.com>
2019-11-20 09:18:06 +0800

    add long description

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name="Chamaeleo",
     version="0.1",
     description="library for the well accepted coding schemes of DNA storage",
+    long_description="Chamaeleo is currently the only collection focused on different codec methods for DNA storage. This kit is mainly developed and operated by BGI-Research (Shenzhen). It provides you a chance to use the classical DNA encoding and decoding methods to save files into DNA motifs or load them from DNA motifs.",
     author="Zhi PING, Hao-Ling ZHANG, Sha (Joe) ZHU",
     author_email="zhanghaoling@genomics.cn",
     url="https://github.com/ntpz870817/Chamaeleo",
