d905228a751b1333c228f12be96cb53d71255f51
HaolingZHANG <hlzchn@gmail.com>
2020-03-04 22:19:37 +0800

    Add Circle CI test and modify the error in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="Chamaeleo",
-    version="1.12",
+    version="1.13",
     description="library for the well accepted coding schemes of DNA storage",
     long_description="Chamaeleo is currently the only collection focused on different codec methods for DNA storage. This kit is mainly developed and operated by BGI-Research (Shenzhen). It provides you a chance to use the classical DNA encoding and decoding methods to save files into DNA sequences or load them from DNA sequences.",
     author="Zhi PING, Hao-Ling ZHANG, Sha (Joe) ZHU",
@@ -28,7 +28,7 @@ setup(
             "data/pictures/Mona Lisa.jpg",
             "data/pictures/United Nations Flag.bmp",
             "data/videos/Exiting the Factory.flv",
-            "data/videos/I have a Dream .mp4",
+            "data/videos/I have a Dream.mp4",
         ]
     },
     package_dir={"Chamaeleo": "."},
