1c22d23b61af6b7f8c00a37c340f0530b9fea528
HaolingZHANG <hlzchn@gmail.com>
2020-04-08 17:27:55 +0800

    revision

--- methods/verifies/rs.py
+++ methods/verifies/rs.py
@@ -132,7 +132,7 @@ class RS:
 
         output_list = []
         for one_byte in v_seq:
-            temp_bits = list(map(int, list(bin(one_byte))[2:]))
+            temp_bits = list(map(int, list(bin(int(str(one_byte), 10)))[2:]))
             temp_bits = [0 for _ in range(8 - len(temp_bits))] + temp_bits
             output_list += temp_bits
 
@@ -201,9 +201,7 @@ class RS:
         try:
             output_list = []
             for one_byte in self.tool.decode(bytearray(byte_list)):
-                print("one_byte = " + str(one_byte))
-                print("type(one_byte) = " + str(type(one_byte)))
-                temp_bits = list(map(int, list(bin(one_byte))[2:]))
+                temp_bits = list(map(int, list(bin(int(str(one_byte), 10)))[2:]))
                 temp_bits = [0 for _ in range(8 - len(temp_bits))] + temp_bits
                 output_list += temp_bits
         except ReedSolomonError:
