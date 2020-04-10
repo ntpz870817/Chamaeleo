17d3b00a292308e55e10f38791e3f6f353c38af5
HaolingZHANG <hlzchn@gmail.com>
2020-04-10 11:35:04 +0800

    update test

--- test/rs_test.py
+++ test/rs_test.py
@@ -19,18 +19,18 @@ class TestEncodeDecode(unittest.TestCase):
             target_list.append(int(str("".join(list(map(str, byte_list[index: index + 8])))), 2))
 
         print("ord list before encode = " + str(target_list))
+        print("type v_seq[0] = " + str(type(target_list[0])))
+        print(bytearray(target_list))
 
-        v_seq = list(self.tool.encode(target_list))
-
+        v_seq = self.tool.encode(target_list)
         print("ord list after encode = " + str(v_seq))
 
-        v_seq = list(self.tool.decode(v_seq))
-
+        v_seq = self.tool.decode(list(v_seq))
         print("ord list after decode = " + str(v_seq))
 
         output_list = []
-        for one_byte in v_seq:
-            temp_bits = list(map(int, list(bin(int(str(one_byte), 10)))[2:]))
+        for one_byte in list(v_seq):
+            temp_bits = list(map(int, list(bin(one_byte))[2:]))
             temp_bits = [0 for _ in range(8 - len(temp_bits))] + temp_bits
             output_list += temp_bits
 
