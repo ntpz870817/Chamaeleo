64140823dfc68e10cd480d1912355b100dda89a5
HaolingZHANG <hlzchn@gmail.com>
2020-04-14 23:40:47 +0800

    modlfy style

--- examples/demo_case_sc.py
+++ examples/demo_case_sc.py
@@ -47,4 +47,3 @@ if __name__ == "__main__":
     matrix_1, _ = data_handle.read_binary_from_all(read_file_path, 120, False)
     matrix_2, _ = data_handle.read_binary_from_all(write_file_path, 120, False)
     print("source digital file == target digital file: " + str(matrix_1 == matrix_2))
-
